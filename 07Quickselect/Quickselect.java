85cde9b8ed87bb528785f8a5dccb8c44b99385ea
Penn Wu <penn.z.wu@gmail.com>
2017-03-20 22:06:05 -0400

    Did Quicksort

--- 07Quickselect/Quickselect.java
+++ 07Quickselect/Quickselect.java
@@ -1,54 +1,43 @@
 import java.util.Arrays;
 public class Quickselect {
-    /*public static int part(int[] data, int start, int end) {
-	System.out.print('{');
-	for (int i = 0; i < data.length; i ++) {
-	    System.out.print(data[i] + ", ");
+    public static void quicksort(int[] nums) {
+	quicksortH(nums, 0, nums.length - 1);
+    }
+    
+    private static void quicksortH(int[] nums, int start, int end) {
+	if (end - start < 2) {
+	    return;
 	}
-	System.out.println('}');
-        int[] parted = new int[data.length];
-	int count = 0;
-	int pivotIndex = (int)(Math.random()*(end - start + 1) + start);
-	int pivot = data[pivotIndex];
-	System.out.println("Partitioning around " + data[pivotIndex]);
-	for (int i = 0; i < data.length; i ++) {
-	    if (data[i] < pivot) {
-		parted[count] = data[i];
-		count ++;
+	else {
+	    int pivotIndex = (int)(Math.random() * (end - start + 1) + start);
+	    int pivot = nums[pivotIndex];
+	    swap(nums, start, pivotIndex);
+	    int left = start + 1;
+	    int middle = start + 1;
+	    int right = end;
+	    while (middle <= right) {
+		if (nums[middle] < pivot) {
+		    if (left != middle) {
+			swap(nums, left, middle);
+		    }
+		    left ++;
+		    middle ++;
+		}
+		else if (nums[middle] == pivot) {
+		    middle ++;
+		}
+		else if (nums[middle] > pivot) {
+		    swap(nums, middle, right);
+		    right --;
+		}
 	    }
+	    pivotIndex = left - 1;
+	    swap(nums, start, pivotIndex);
+	    quicksortH(nums, start, left - 1);
+	    quicksortH(nums, middle - 1, end);
 	}
-	System.out.print('{');
-	for (int i = 0; i < data.length; i ++) {
-	    System.out.print(parted[i] + ", ");
-	}
-	System.out.println('}');
-	System.out.println(count);
-	parted[count] = pivot;
-	pivotIndex = count;
-	count ++;
-	boolean ignoreOnce = true;
-	for (int i = start; i <= end; i ++) {
-	    System.out.print('{');
-	    for (int j = 0; j < data.length; j ++) {
-		System.out.print(parted[j] + ", ");
-	    }
-	    System.out.println('}');
-	    if (data[i] == pivot && ignoreOnce) {
-		ignoreOnce = false;
-	    }
-	    else if (data[i] >= pivot) {
-		parted[count] = data[i];
-		count ++;
-	    }
-	}
-	System.out.print('{');
-	for (int i = start; i <= end; i ++) {
-	    System.out.print(parted[i] + ", ");
-	}
-	System.out.println('}');
-	return pivotIndex;
-	}*/
-
+    }
+    
     public static int quickselect(int[] nums, int k) {
 	int start = 0;
 	int end = nums.length - 1;
@@ -65,27 +54,11 @@ public class Quickselect {
     }
 
     public static int part(int[] nums, int start, int end) {
-	/*System.out.println("Original array");
-	System.out.print('{');
-	for (int i = 0; i < nums.length; i ++) {
-	    System.out.print(nums[i] + ", ");
-	}
-	System.out.println('}');
-	System.out.println("----------");*/
-	
 	int pivotIndex = (int)(Math.random() * (end - start + 1) + start);
 	int pivot = nums[pivotIndex];
 	swap(nums, start, pivotIndex);
+	
 	pivotIndex = start;
-
-	/*System.out.println("Swapping pivot with beginning");
-	System.out.print('{');
-	for (int i = 0; i < nums.length; i ++) {
-	    System.out.print(nums[i] + ", ");
-	}
-	System.out.println('}');
-	System.out.println("----------");*/
-
 	int left = start + 1;
 	int middle = start + 1;
 	int right = end;
@@ -104,25 +77,10 @@ public class Quickselect {
 		swap(nums, middle, right);
 		right --;
 	    }
-	    /*System.out.print('{');
-	    for (int j = 0; j < nums.length; j ++) {
-	    System.out.print(nums[j] + ", ");
-	    }
-	    System.out.println('}');*/
 	}
 
 	pivotIndex = left - 1;
-	swap (nums, start, pivotIndex);
-	
-	/*System.out.println("Final array");
-	System.out.println("Left: " + left);
-	System.out.println("Middle: " + middle);
-	System.out.println("Right: " + right);
-	System.out.print('{');
-	for (int i = 0; i < nums.length; i ++) {
-	    System.out.print(nums[i] + ", ");
-	}
-	System.out.println('}');*/
+	swap(nums, start, pivotIndex);
 	return pivotIndex;
     }
 
@@ -133,13 +91,24 @@ public class Quickselect {
     }
     
     public static void main(String[] args) {
-	int[] blah = {3, 12, 2, 3, 7, 89, -1, 23, 67, 3, 3, 3, 1, -5, 7, 3, -2, 7, 86, 4};
-	int[] unsorted = {6, 5, 3, 1, 8, 7, 2, 4};
-	int[] a = {999,999,999,4,1,0,3,2,999,999,999,5,3,3,3,3};
-	int[] b = {9, 4, 3, 6, 5, 1, 0, 8, 7, 2};
-	int i = Integer.parseInt(args[0]);
-	System.out.println(quickselect(a, i));
-        Arrays.sort(a);
-	System.out.println(a[i]);
+	int[] u = new int[50];
+	for (int i = 0; i < u.length; i ++) {
+	    u[i] = (int)(Math.random() * 201 - 100);
+	}
+	int r = (int)(Math.random() * u.length);
+	System.out.println(quickselect(u, r));
+	/*quicksort(u);
+	System.out.print("{");
+	for (int i = 0; i < u.length; i ++) {
+	    System.out.print(u[i] + ", ");
+	}
+	System.out.println("}");*/
+        Arrays.sort(u);
+	System.out.println(u[r]);
+	/*System.out.print("{");
+	for (int i = 0; i < u.length; i ++) {
+	    System.out.print(u[i] + ", ");
+	}
+	System.out.println("}");*/
     }
 }
