62c3e7d918a9ea31d6ba8cce83f74d430be8e8e2
Penn Wu <penn.z.wu@gmail.com>
2017-03-28 20:27:38 -0400

    Finished LinkedList

--- 09LinkedList/MyLinkedList.java
+++ 09LinkedList/MyLinkedList.java
@@ -17,17 +17,17 @@ public class MyLinkedList {
     }
 
     public MyLinkedList() {
+	// no initialization required, just add ints
+    }
+
+    public int size() {
+	return size;
     }
 
     public boolean add(int value) {
-	start = new LNode(value, start);
-	size ++;
-	return true;
-    }
-
-    public boolean addTemp(int value) {
-	if (start == null) {
+        if (start == null) {
 	    start = new LNode(value);
+	    size ++;
 	    return true;
 	}
 	else {
@@ -36,29 +36,55 @@ public class MyLinkedList {
 		current = current.next;
 	    }
 	    current.next = new LNode(value);
+	    size ++;
 	    return true;
 	}
     }
 
-    public int size() {
-	return size;
+    public void add(int index, int value) {
+	if (index < 0 || index > size) {
+	    throw new IndexOutOfBoundsException("Attempted to add to an invalid index!");
+	}
+	if (size == 0) {
+	    start = new LNode(value);
+	    size ++;
+	    return;
+	}
+	LNode current = start;
+	for (int i = 0; i < index - 1; i ++) {
+	    current = current.next;
+	}
+	LNode nodeAtIndex = current.next;
+        LNode insert = new LNode(value, nodeAtIndex);
+	current.next = insert;
+	size ++;
     }
 
-    public String toString() {
-	if (start == null) {
-	    return "[]";
+    public int remove(int index) {
+	if (index < 0 || index >= size) {
+	    throw new IndexOutOfBoundsException("Attempted to remove an invalid index!");
 	}
-	String s = "[";
-	LNode currentNode = start;
-	for (int i = 0; i < size - 1; i ++) {
-	    s += currentNode.data + ", ";
-	    currentNode = currentNode.next;
+	if (index == 0) {
+	    int ret = start.data;
+	    start = start.next;
+	    size --;
+	    return ret;
 	}
-	s += currentNode.data + "]";
-	return s;
+	LNode current = start;
+	for (int i = 0; i < index - 1; i ++) {
+	    current = current.next;
+	}
+	LNode nodeAtIndex = current.next;
+        int removed = nodeAtIndex.data;
+	current.next = current.next.next;
+	size --;
+	return removed;
     }
 
     public int get(int index) {
+	if (index < 0 || index >= size) {
+	    throw new IndexOutOfBoundsException("Attempted to set an invalid index!");
+	}
 	LNode currentNode = start;
 	for (int i = 0; i < index; i ++) {
 	    currentNode = currentNode.next;
@@ -67,6 +93,9 @@ public class MyLinkedList {
     }
 
     public int set(int index, int newValue) {
+	if (index < 0 || index >= size) {
+	    throw new IndexOutOfBoundsException("Attempted to get an invalid index!");
+	}
 	LNode currentNode = start;
 	for (int i = 0; i < index; i ++) {
 	    currentNode = currentNode.next;
@@ -76,15 +105,54 @@ public class MyLinkedList {
 	return oldValue;
     }
 
+    public int indexOf(int value) {
+	LNode current = start;
+	for (int i = 0; i < size; i ++) {
+	    if (current.data == value) {
+		return i;
+	    }
+	    current = current.next;
+	}
+	return -1;
+    }
+
+    public String toString() {
+	if (start == null) {
+	    return "[]";
+	}
+	String s = "[";
+	LNode current = start;
+	while (current.next != null) {
+	    s += current.data + ", ";
+	    current = current.next;
+	}
+	s += current.data + "]";
+	return s;
+    }
+
     public static void main(String[] args) {
 	MyLinkedList Penn = new MyLinkedList();
+	Penn.add(-1);
 	System.out.println(Penn);
+	System.out.println(Penn.remove(0));
 	for (int i = 0; i < 10; i ++) {
 	    int rand = (int)(Math.random() * 10);
 	    System.out.print("Adding " + rand + ": ");
-	    System.out.println(Penn.addTemp(rand));
+	    System.out.println(Penn.add(rand));
+	    System.out.println(Penn);
+	    if (i % 2 == 0) {
+		System.out.print("Removing index 0: ");
+		System.out.println(Penn.remove(0));
+		System.out.println(Penn);
+	    }
+	}
+	Penn.add(5, -1);
+	System.out.println(Penn);
+	for (int i = 0; i < Penn.size(); i ++) {
+	    int rand = (int)(Math.random() * 10);
+	    System.out.println("Setting " + Penn.get(i) + " to " + rand);
+	    System.out.println(Penn.set(i, rand));
 	    System.out.println(Penn);
 	}
-	System.out.println(Penn);
     }
 }
