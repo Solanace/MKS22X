10b8d43ffa70998df3a28f50d8dc2f73f5672eda
Penn Wu <penn.z.wu@gmail.com>
2017-04-16 01:02:18 -0400

    Finished Deque...probably

--- 10Deque/MyDeque.java
+++ 10Deque/MyDeque.java
@@ -1,6 +1,6 @@
 import java.util.NoSuchElementException;
 public class MyDeque {
-    private int front, back, size; // back is exclusive
+    private int front, back, size; // inclusive front, exclusive back
     private String[] arr;
 
     public MyDeque() {
@@ -46,6 +46,63 @@ public class MyDeque {
 	}
     }
 
+    public String removeFirst() {
+	if (isEmpty()) {
+	    throw new NullPointerException();
+	}
+	else {
+	    String ret = arr[front];
+	    if (front == arr.length - 1) {
+		front = 0;
+	    }
+	    else {
+		front ++;
+	    }
+	    size --;
+	    return ret;
+	}
+    }
+
+    public String removeLast() {
+	if (isEmpty()) {
+	    throw new NullPointerException();
+	}
+	else {
+	    if (back == 0) {
+		back = arr.length - 1;
+	    }
+	    else {
+		back --;
+	    }
+	    size --;
+	    String ret = arr[back];
+	    return ret;
+	}
+    }
+
+    public String getFirst() {
+	if (isEmpty()) {
+	    throw new NullPointerException();
+	}
+	else {
+	    return arr[front];
+	}
+    }
+
+    public String getLast() {
+	if (isEmpty()) {
+	    throw new NullPointerException();
+	}
+	else {
+	    if (back == 0) {
+		return arr[arr.length - 1];
+	    }
+	    else {
+		return arr[back - 1];
+	    }
+	}
+    }
+
     private boolean isEmpty() {
 	return front == back && size == 0;
     }
@@ -55,13 +112,14 @@ public class MyDeque {
     }
 
     private void grow() {
+	// only grows full arrays
 	String[] newArr = new String[arr.length * 2];
 	int count = 0;
 	for (int i = front; i < arr.length; i ++) {
 	    newArr[count] = arr[i];
 	    count ++;
 	}
-	for (int i = back; i < front; i ++) {
+	for (int i = 0; i < back; i ++) {
 	    newArr[count] = arr[i];
 	    count ++;
 	}
@@ -71,9 +129,12 @@ public class MyDeque {
     }
 
     public static void main(String[] args) {
-	int[] a = {0, 1, 76, 34, 2};
-	for (int i = 0; i < a.length; i ++) {
-	    System.out.println(a[i]);
+        MyDeque Penn = new MyDeque();
+        for (int i = 0; i < 100; i ++) {
+	    Penn.addLast("" + i);
+	}
+	for (int i = 0; i < 100; i ++) {
+	    System.out.println(Penn.removeLast());
 	}
     }
 }
