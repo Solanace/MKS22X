4ded8a397cf847e928ee0735dc740453ad091e26
Penn Wu <penn.z.wu@gmail.com>
2017-04-24 20:55:53 -0400

    Added in test cases

--- 11StackCalculations/StackCalc.java
+++ 11StackCalculations/StackCalc.java
@@ -1,5 +1,5 @@
 import java.util.Stack;
-public class Postfix {
+public class StackCalc {
     public static double eval(String s) {
 	String[] arr = s.split(" ");
 	Stack<Double> sta = new Stack<Double>();
@@ -43,8 +43,11 @@ public class Postfix {
     }
 
     public static void main(String[] args) {
-	System.out.println(eval("10 2.0 +"));
-	System.out.println(eval("11 3 - 4 + 2.5 *"));
-	System.out.println(eval("8 2 + 99 9 - * 2 + 9 -"));
+        System.out.println(StackCalc.eval("10 2 +"));//12.0
+	System.out.println(StackCalc.eval("10 2 -"));//8.0
+	System.out.println(StackCalc.eval("10 2.0 +"));//12.0
+	System.out.println(StackCalc.eval("11 3 - 4 + 2.5 *"));//30.0
+	System.out.println(StackCalc.eval("8 2 + 99 9 - * 2 + 9 -"));//839.0
+	System.out.println(StackCalc.eval("10 2 + 10 * 1 + 1 1 1 + + + 10 10 + -"));//104.0
     }
 }
